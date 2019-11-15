9ae241d12ebe493b3c4f4c4cd0fc4a3e5220da90
harsh6768 <harshchaurasiya6768@gmail.com>
2019-11-15 16:07:27 +0530

    manifest file added

---
+++ Server/manifest.js
@@ -0,0 +1,53 @@
+const Inert                     =       require('@hapi/inert');
+const Vision                    =       require('@hapi/vision');
+const HapiSwagger               =       require('hapi-swagger');
+const mongoDb                   =       require('./Model/mongo.db');
+const admin                     =       require('./Model/admin');
+
+//creat admin
+admin.createAdmin();
+
+const swaggerOptions = {
+
+    info: {
+        description: 'This is a Cab Booking server',
+        title: 'Cab Booking API',
+        version : '0.0.1',
+        contact: {
+            email : 'harshchaurasiya6768@gmail.com'
+        }
+    },
+    grouping: 'tags'
+
+}
+
+const manifest = {
+    server: {
+        port: 5000
+    },
+    register: {
+        plugins: [
+                Inert,
+                Vision,
+                {
+                    plugin: HapiSwagger,
+                    options: swaggerOptions
+                },
+                {
+                    plugin:require('../Server/Modules/admin/index')
+                },
+                {
+                    plugin:require('../Server/Modules/user/index')
+                },
+                {
+                    plugin:require('../Server/Modules/driver/index')
+                }
+        ],
+        options: {
+            once: true
+        }
+    }
+}
+
+module.exports=manifest;
+
--- server.js
+++ server.js
@@ -1,59 +1,19 @@
-const Hapi                        =       require('@hapi/hapi');
-const Inert                       =       require("@hapi/inert");
-const Good                        =       require("@hapi/good");
-const Vision                      =       require("@hapi/vision");
-const HapiSwagger                 =       require("hapi-swagger");
-const admin                       =       require("./Server/Model/admin");
-const mongoDb                     =       require("./Server/Model/mongo.db");
 
-// //for creating the admin
-admin.createAdmin();
+const Glue                      =       require('@hapi/glue');
+const manifest                  =       require('./Server/manifest');
 
-const port = process.env.port || 5000;
-
-const init=async()=>{
-
-    const server = await new Hapi.Server({
-      host: "localhost",
-      port
-    });
-    
-    //swagger documentation options
-    const swaggerOptions = {
-      info: {
-        title: "Test API Documentation",
-        version: "0.0.1"
-      }
-    };
-    //register dependencies
-    await server.register([
-      Inert,
-      Vision,
-      Good,
-      {
-        plugin: HapiSwagger,
-        options: swaggerOptions
-      },
-      // {
-      //   plugin:require('./Server/Routes/routes')
-      // }
-      {
-         plugin:require('./Server/Modules/user/index')
-      },
-      {
-         plugin:require('./Server/Modules/admin/index')
-      },
-      {
-        plugin:require('./Server/Modules/driver/index')
-      }
-    ]);
-
-    try {
-      await server.start();
-      console.log("Server running at:", server.info.uri);
-    } catch (err) {
-      console.log(err);
-    }
-
+const options = {
+    relativeTo: __dirname
 };
-init();
+
+(async()=>{
+    try {
+        const server = await Glue.compose(manifest, options);
+        await server.start();
+        console.log('hapi days!');
+    }
+    catch (err) {
+        console.error(err);
+        process.exit(1);
+    }
+})();

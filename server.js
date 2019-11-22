e183b17f660a2d80044dbb105850430dc30dc1f7
harsh6768 <harshchaurasiya6768@gmail.com>
2019-11-22 11:13:52 +0530

    console added server running port

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ const options = {
         const server = await Glue.compose(manifest, options);
         await server.start();
         console.log('hapi days!');
+        console.log('Server running at: %s://%s:%s', server.info.protocol, server.info.address, server.info.port);
+  
     }
     catch (err) {
         console.error(err);
